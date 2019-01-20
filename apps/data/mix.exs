c7ee3771f9d6e82f2a2c1e03a6a6d602c4bbc09a
maneptha <maneptha@gmail.com>
2019-01-20 14:57:49 +0100

    Sanitize filename to form usable in browser.

--- apps/data/mix.exs
+++ apps/data/mix.exs
@@ -50,7 +50,8 @@ defmodule Data.MixProject do
       {:dataloader, "~> 1.0"},
       {:arc, "~> 0.11.0"},
       {:arc_ecto, "~> 0.11.1"},
-      {:arc_gcs, "~> 0.1.0", only: :prod}
+      {:arc_gcs, "~> 0.1.0", only: :prod},
+      {:zarex, "~> 1.0"}
     ]
   end
 
