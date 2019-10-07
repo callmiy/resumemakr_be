021626de83a6c28d39a15a283c2f1f8fb0f21b80
maneptha <maneptha@gmail.com>
2019-10-07 19:35:08 +0200

    Updated config

--- apps/rm_emails/config/config.exs
+++ apps/rm_emails/config/config.exs
@@ -2,12 +2,14 @@ use Mix.Config
 
 config :rm_emails, RMEmails.DefaultImpl.Mailer,
   adapter: Swoosh.Adapters.SMTP,
-  relay: System.get_env("RESUME_MAKR_SMTP_RELAY"),
-  username: System.get_env("RESUME_MAKR_SMTP_USER"),
-  password: System.get_env("RESUME_MAKR_SMTP_PASS"),
+  relay: System.get_env("RESUME_MAKR_SMTP_RELAY") || "smtp.ethereal.email",
+  username: System.get_env("RESUME_MAKR_SMTP_USER") || "loyal.farrell47@ethereal.email",
+  password: System.get_env("RESUME_MAKR_SMTP_PASS") || "BxXEwfa5B7zfDHY941",
   tls: :always,
   auth: :always,
-  port: System.get_env("RESUME_MAKR_SMTP_PORT") |> String.to_integer()
+  port: System.get_env("RESUME_MAKR_SMTP_PORT")
+         |> Kernel.||("587")
+  |> String.to_integer()
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
