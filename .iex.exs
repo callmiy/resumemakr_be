67071c540c55bbad8670449f7a41b7ff7a63fc68
maneptha <maneptha@gmail.com>
2019-10-08 07:20:58 +0200

    No custom .iex.exs file.

--- .iex.exs
+++ .iex.exs
@@ -7,5 +7,11 @@ alias Data.Resumes.Skill
 alias Data.Resumes.Resume
 alias Data.Accounts
 alias Data.Accounts.User
+alias Data.Accounts.Credential
 alias Data.Repo
 alias Data.Uploaders.ResumePhoto
+
+alias Data.FactoryResume
+alias Data.FactoryUser
+alias Data.Factory
+alias Data.FactoryRegistration
